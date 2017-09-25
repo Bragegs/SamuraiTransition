b372703ed7b6f9c542d88d35bb34681531f49b80
Brage G. Staven <bragestaven@gmail.com>
2017-09-25 15:25:02 +0200

    swift 4

--- SamuraiTransition/CircleZanConfig.swift
+++ SamuraiTransition/CircleZanConfig.swift
@@ -44,8 +44,8 @@ extension CircleZanConfig {
     
     fileprivate func circleAreaPath() -> UIBezierPath {
         let path = UIBezierPath()
-        let startAngle = -CGFloat(M_PI_2)
-        let endAngle = CGFloat(M_PI) + CGFloat(M_PI_2)
+        let startAngle = -CGFloat(CGFloat.pi / 2)
+        let endAngle = CGFloat(CGFloat.pi / 2) + CGFloat(CGFloat.pi / 2)
         path.addArc(withCenter: zanPoint, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
         return path
     }
